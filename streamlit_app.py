df289a645980273e58fa361a318fe9445a540f28
Vivek Shah <46943926+DTVivek@users.noreply.github.com>
2023-03-02 21:29:17 -0600

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('BREAKFAST MENU')
+
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale & Orange Smoothie')
+streamlit.text('Egg')
