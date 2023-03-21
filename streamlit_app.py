6b252c98ecd07a2919424abe5b63f9509cd6acfa
Vivek Shah <46943926+DTVivek@users.noreply.github.com>
2023-03-20 19:24:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,11 +37,11 @@ try:
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
 else:
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # Normalised Data
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # convert into tables
-streamlit.dataframe(fruityvice_normalized)
+  streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
 streamlit.error()
